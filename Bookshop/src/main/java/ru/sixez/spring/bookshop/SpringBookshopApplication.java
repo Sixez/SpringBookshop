a462f9b31e862f01160ac10ea35cd89444af93e8
Димка <sixez69@gmail.com>
2021-02-27 21:33:25 +0300

    Added Bookshop module, created a main class and test for it

---
+++ Bookshop/src/main/java/ru/sixez/spring/bookshop/SpringBookshopApplication.java
@@ -0,0 +1,14 @@
+package ru.sixez.spring.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookshopApplication {
+    /**
+     * Starting Spring Boot application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBookshopApplication.class, args);
+    }
+}
---
+++ Bookshop/src/test/java/ru/sixez/spring/bookshop/SpringBookshopApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.sixez.spring.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBookshopApplicationTest {
+    /**
+     * Testing if application is stating at all
+     */
+    @Test
+    public void contextLoads() {
+    }
+}
